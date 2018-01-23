a11ec355c64fb877c5b1b59e0960cef6ac55b9a2
FleurTamboezer <ftamboezer@gmail.com>
2018-01-23 11:15:27 +0100

    Fixed data/date issue

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,14 +1,15 @@
 class Atm
+  require 'date'
   attr_accessor :funds
+
   def initialize
     @funds = 1000
   end
 
   def withdraw(amount, account)
-    require 'date'
     case
     when insuffient_funds_in_account?(amount, account)
-      { status: false, message: 'insuffient funds', date: Data.today }
+      { status: false, message: 'insuffient funds', date: Date.today }
     else
       perform_transaction(amount, account)
     end
