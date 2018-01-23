90969c4455b2d2f7340262f87e7dee7b7d12c470
kalle <kmthorsnes@gmail.com>
2018-01-23 10:20:09 +0100

    Added method to check if funds are sufficient. Step #3

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,11 +1,20 @@
+
 class Atm
   attr_accessor :funds
-
+require 'date'
   def initialize
     @funds = 1000
   end
 
-  def withdraw(amount)
-    @funds -= amount
+  def withdraw(amount, account)
+    case
+    when amount > account.balance
+      return
+    else
+      @funds -= amount
+      account.balance = account.balance - amount
+      { status: true, message: 'success', date: Date.today, amount: amount }
+
+    end
   end
 end
