d333b41ff62eeeb7945d8e397e89a62143522a70
kalle <kmthorsnes@gmail.com>
2018-01-24 13:51:09 +0100

    Trying to set method for passing stage #3

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,25 @@
+# account.rb
+require 'date'
+
 class Account
-attr_accessor :pin_code, :account_status
+
+  attr_accessor :pin_code, :account_status
+
+  STANDARD_VALIDITY_YRS = 5
 
   def initialize
     @pin_code = generate_pin
     @account_status = :active
+    @exp_date = exp_date
   end
 
   private
+
   def generate_pin
     rand(1000..9999)
   end
 
-
-
+  def exp_date
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,6 @@
 require './lib/account.rb'
 
 describe Account do
-
   it 'is expected to have a 4 digit pincode' do
     pin_length = Math.log10(subject.pin_code).to_i + 1
     expect(pin_length).to eq 4
@@ -10,5 +9,9 @@ describe Account do
   it 'is expected to have :active status on initialize' do
     expect(subject.account_status).to eq :active
   end
-  
+
+  it 'is expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime('%m/%y')
+    expect(subject.exp_date).to eq expected_date
+  end
 end
