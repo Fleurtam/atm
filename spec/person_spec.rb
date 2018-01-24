54d33f7e2a181b7a90dc2cfb641d0ffa6d0c2cc1
kalle <kmthorsnes@gmail.com>
2018-01-24 15:31:04 +0100

    Handing over the wheel to Fleur, stage 7

---
+++ spec/person_spec.rb
@@ -0,0 +1,11 @@
+require './lib/person.rb'
+require './lib/atm.rb'
+require './lib/account.rb'
+
+describe Person do
+  subject { described_class.new(name: 'T&T') }
+
+  it 'is expected to have a :name on initialize' do
+    expect(subject.name).not_to be nil
+  end
+end
