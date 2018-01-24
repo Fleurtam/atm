788b353ffbaf418c3e66f837da5ec5331b7443bd
FleurTamboezer <ftamboezer@gmail.com>
2018-01-24 12:24:45 +0100

    Added method for checking if account status is active

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,16 @@
 class Account
-attr_accessor :pin_code
+attr_accessor :pin_code, :account_status
 
   def initialize
     @pin_code = generate_pin
+    @account_status = :active
   end
 
   private
   def generate_pin
     rand(1000..9999)
   end
+
+
+
 end
