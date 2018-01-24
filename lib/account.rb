ec4bf7d9350ad667f8040512f5515bc53e7cb021
kalle <kmthorsnes@gmail.com>
2018-01-24 15:09:07 +0100

    Added owner methods and missing owner method

--- lib/account.rb
+++ lib/account.rb
@@ -2,14 +2,16 @@
 require 'date'
 
 class Account
-  attr_accessor :pin_code, :account_status
+  attr_accessor :pin_code, :account_status, :owner
 
   STANDARD_VALIDITY_YRS = 5
 
-  def initialize
+  def initialize(attrs = {})
     @pin_code = generate_pin
     @account_status = :active
     @exp_date = exp_date
+    set_owner(attrs[:owner])
+
   end
 
   def exp_date
@@ -24,5 +26,13 @@ class Account
 
   def generate_pin
     rand(1000..9999)
+  end
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise 'An Account owner is required'
   end
 end
